d009f975b92964be22ac66ee114499a661df4882
nayan458 <nayancoding@gmail.com>
2025-08-25 20:17:06 +0530

    configured the nest js config

---
+++ apps/api-gateway/src/main.ts
@@ -0,0 +1,15 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+require('module-alias/register')
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+import { init } from '@stone-inscription-backend/nestjs-config'
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await init(app);
+}
+
+bootstrap();
