04197ca5cad986fc92b80a29999a7c1f56de6b9a
nayan458 <nayancoding@gmail.com>
2025-08-25 19:58:48 +0530

    added auth microservice

---
+++ lib/nestjs-config/src/lib/bootstrap/init.ts
@@ -0,0 +1,15 @@
+import { INestApplication, Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import cookieParser = require('cookie-parser');
+
+export async function init(app: INestApplication) {
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.use(cookieParser());
+  const port = app.get(ConfigService).getOrThrow('PORT');
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
+}
