18e69276e1d3e891db3aace5196bbd31d39c4eb3
nayan458 <nayancoding@gmail.com>
2025-08-28 13:59:15 +0530

    api-gateway env configuration resolved

--- apps/api-gateway/src/app/app.module.ts
+++ apps/api-gateway/src/app/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ImageUploaderModule } from './imageUploader/image-uploader.module';
+import { ConfigModule } from '@nestjs/config';
+import path from 'path';
 
 @Module({
-  imports: [ImageUploaderModule],
+  imports: [
+    ImageUploaderModule,
+    ConfigModule.forRoot({
+      envFilePath: [
+        path.resolve(process.cwd(), 'apps/api-gateway/.env'),
+        '.env',
+      ],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
